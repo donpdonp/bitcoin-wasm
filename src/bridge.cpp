ec6366fcb9f0f5e1e2b2e921587fb8c7a529a267
Don Park <donp@donp.org>
2018-02-28 14:27:42 +0700

    do stuff with integers

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -6,9 +6,10 @@
 
 extern "C" {
 
+void printStack(std::vector<std::vector<unsigned char> > stack) ;
 bool scriptRun(int idx)
 {
-  printf("scriptRun %d\n", idx);
+  printf("scriptRun #%d begin\n", idx);
   CScript c = scripts.at(idx);
   //bool EvalScript(vector<vector<unsigned char> >& stack, const CScript& script, unsigned int flags, const BaseSignatureChecker& checker, ScriptError* serror)
   std::vector<std::vector<unsigned char> > stack;
@@ -18,24 +19,46 @@ bool scriptRun(int idx)
   const TransactionSignatureChecker& checker = TransactionSignatureChecker(&tx, nIn);
   bool retval = EvalScript(stack, c, SCRIPT_VERIFY_P2SH, checker, &error);
   if (retval == 0) {
-      printf("jsgo err: %s\n", ScriptErrorString(error));
+      printf("scriptRun err: %s\n", ScriptErrorString(error));
   } else {
-      printf("jsgo GOOD\n");
+      printf("scriptRun GOOD\n");
   }
+  printStack(stack);
   return retval;
 }
 
+void printStack(std::vector<std::vector<unsigned char> > stack) {
+  printf("scriptRun stack size %d:\n", stack.size());
+  for(std::vector<unsigned char>::size_type i = 0; i != stack.size(); i++) {
+    std::vector<unsigned char> strvec = stack.at(i);
+    std::string str(strvec.begin(), strvec.end());
+    printf("scriptRun stack position #%d: %s %d\n", i, str.c_str(), str.c_str()[0]);
+  }
+}
+
 const int stringCompile(char** opcodeNames, int len){
-  printf("stringCompile %d opcode strings \n", sizeof(char*), (int)opcodeNames, len);
+  printf("stringCompile %d opcode strings \n", len);
   CScript c = CScript();
   for(int i=0; i<len; i++) {
     char* opcodeName = opcodeNames[i];
-    opcodetype opcode = opStringToOpCode(opcodeName);
-    if(opcode != OP_INVALIDOPCODE) {
-      c << opcode;
-      printf("#%d %s ADDED\n", i, opcodeName);
-    } else {
-      printf("#%d %s invalid\n", i, opcodeName);
+    if(strlen(opcodeName) > 0) {
+      opcodetype opcode = opStringToOpCode(opcodeName);
+      if(opcode != OP_INVALIDOPCODE) {
+        c << opcode;
+        printf("#%d %s opcode\n", i, opcodeName);
+      } else {
+        std::string str(opcodeName);
+        if (is_digits(str)) {
+          int num = std::stoi(str);
+          c << CScriptNum(num);
+          printf("#%d %d number\n", i, num);
+        } else {
+          std::vector<unsigned char> vectorString;
+          std::copy(str.begin(), str.end(), std::back_inserter(vectorString));
+          c << vectorString;
+          printf("#%d %s value\n", i, opcodeName);
+        }
+      }
     }
   }
   scripts.push_back(c);
@@ -56,6 +79,11 @@ const opcodetype opStringToOpCode(char* opName) {
     }
   }
   return OP_INVALIDOPCODE;
+}
+
+bool is_digits(const std::string &str)
+{
+    return std::all_of(str.begin(), str.end(), ::isdigit); // C++11
 }
 
 }
